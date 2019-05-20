5119ce9955ace0496030e5e942a6625b01b360c2
Gabriel Blaine Palmer <gabepalmer@Gabriels-MacBook-Air.local>
2019-05-20 07:56:13 -0600

    Finalized code to be submitted

--- Coding Challenge 2/Coding Challenge 2/MealTableViewCell.swift
+++ Coding Challenge 2/Coding Challenge 2/MealTableViewCell.swift
@@ -13,16 +13,5 @@ class MealTableViewCell: UITableViewCell {
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var caloriesLabel: UILabel!
     @IBOutlet weak var ratingLabel: UILabel!
-    
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
 
 }
